e10c8dbed2e9f9c4d7120ce52c3da679d100ca86
Jared Shakespaere <jaredshakespaere@C22074.local>
2022-06-27 16:58:21 -0500

    displaying the error message now instead of the calculation description

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,5 +4,6 @@ let currentResult = 0;
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
 let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
+let errorMessage = 'An error' + 'occured!';
 
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, errorMessage);
